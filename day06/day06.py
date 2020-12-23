12ab0db338b9a5ffd0aed8562c0f000b7ced3266
Dileep Kishore <k.dileep1994@gmail.com>
2020-12-23 13:22:06 -0500

    Add: Questions for day07-12

---
+++ day06/day06.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+
+class Bin:
+    """
+    Some bin information
+
+    Parameters
+    ----------
+    bin_string : str
+
+    Attributes
+    ----------
+    """
+
+    def __init__(self, bin_string: str) -> None:
+        pass
